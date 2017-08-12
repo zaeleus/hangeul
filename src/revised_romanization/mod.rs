c6994dfbace6449b6c2c80c250267b66784e84d5
Michael Macias <zaeleus@gmail.com>
2017-08-12 16:17:14 -0500

    Transcribe "ㄹㄹ" as "ll"

--- src/revised_romanization/mod.rs
+++ src/revised_romanization/mod.rs
@@ -10,15 +10,30 @@ pub fn transcribe(word: &Word) -> String {
     let mut it = syllables.iter().peekable();
     let mut res = String::new();
 
+    let mut skip = false;
+
     while let Some(s) = it.next() {
         let (j, k, m) = s.decompose();
 
-        res.push_str(transliterate_initial_consonant(j));
+        if !skip {
+            res.push_str(transliterate_initial_consonant(j));
+        } else {
+            skip = false;
+        }
+
         res.push_str(transliterate_medial_vowel(k));
 
         if let Some(m) = m {
-            /* let t = it.peek();
-            let n = t.map(|t| t.choseong()); */
+            let t = it.peek();
+
+            if let Some(n) = t.map(|t| t.choseong()) {
+                if m == 'ㄹ' && n == 'ㄹ' {
+                    res.push_str("ll");
+                    skip = true;
+                    continue;
+                }
+            }
+
             res.push_str(transliterate_final_consonant(m));
         }
     }
